2caf4ec027be70495aab41020ddf2d0de4991f50
Anvesh Muppeda <muppedaanvesh@gmail.com>
2023-11-02 20:57:56 -0500

    Updating Day 17 notes

---
+++ classes/Docker/Day17/image_v2/script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sum=$(expr $a + $b)
+echo "The sum of a and b is:: $sum"
+sum=$(expr $a + $b)
+echo "The sum of a and b is:: $sum"
+
+sum=$(expr $a + $b)
+echo "The sum of a and b is:: $sum"
+
+sum=$(expr $a + $b)
+echo "The sum of a and b is:: $sum"
